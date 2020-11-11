591492675675f19431431c3acacaec682e930672
syrine <syrinebenjemaa8@gmail.com>
2020-11-11 11:16:44 +0100

    first commit

---
+++ app/src/main/java/com/example/two_activities/SecondActivity.java
@@ -0,0 +1,40 @@
+package com.example.two_activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.BreakIterator;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private static final String EXTRA_REPLY = "";
+    public static final String EXTRA_REPLY =
+            "com.example.android.twoactivities.extra.REPLY";
+    private EditText mReply;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.text_message);
+        textView.setText(message);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+    public void returnReply(View view) {
+        BreakIterator mReply;
+        String reply = mReply.getText().toString();
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, replyIntent);
+        finish();
+    }
+
+
+}
